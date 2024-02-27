f0b5270929a479f69be26d31a9dccfa0c172db72
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2024-02-27 14:45:15 +0200

    Add edit feature for notes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './NoteList';
 import { NoteLayout } from './NoteLayout';
 import { Note } from './Note';
+import { EditNote } from './EditNote';
 
 export type Note = {
   id: string;
@@ -60,6 +61,16 @@ function App() {
     setTags(prev => [...prev, tag]);
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id)
+          return { ...note, ...data, tagIds: tags.map(tag => tag.id) };
+        return note;
+      });
+    });
+  }
+
   return (
     <Container className="my-4">
       <Routes>
@@ -80,7 +91,16 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
           <Route index element={<Note />} />
-          <Route path="edit" element={<h1>Edit</h1>} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
       </Routes>
     </Container>
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -9,12 +9,19 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = '',
+  markdown = '',
+  tags = [],
+}: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
   const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
@@ -35,7 +42,11 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required ref={titleRef}></Form.Control>
+              <Form.Control
+                required
+                ref={titleRef}
+                defaultValue={title}
+              ></Form.Control>
             </Form.Group>
           </Col>
           <Col>
@@ -72,6 +83,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
             as="textarea"
             rows={15}
             ref={markdownRef}
+            defaultValue={markdown}
           ></Form.Control>
         </Form.Group>
 
